bfad370cea592a256e87e721cbf100ed800e3502
amandaalmada <amandaalmadapy@gmail.com>
2022-06-27 10:45:20 -0400

    Primer commit hasta la funcion del contador

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+	// export let name;
+	let saludo = "Hola Silvi y Ana"
+	let contador = 0
+	const aumentar = () => {
+		contador ++;
+	}
+</script>
+
+<main>
+	<h1>{saludo}</h1>
+	<h2>contador: {contador}</h2>
+	<button on:click={aumentar}>Aumentar</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
